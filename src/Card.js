08ef0ef2e0ca0ef81b554f75c840c9c26b0b69a4
Shashank Dwivedi <dwivedishashank31@gmail.com>
2024-01-19 16:45:39 +0530

    Card.js updated

--- src/Card.js
+++ src/Card.js
@@ -2,26 +2,10 @@ import React from 'react';
 import "./Card.css" ;
 
 const Card = ({ heading, description, link }) => {
-    const handleLinkClick = async() => {
-        try {
-          // Make a POST request to the backend endpoint
-            const response = await fetch('https://chatbotify-y83w.onrender.com/card_clicked/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ link }),
-            });
-
-            console.log(response);  // Handle the response as needed
-        } catch (error) {
-          console.error('Error making POST request:', error);
-            }
-        };
     return (
         <div  className='cards'>
 
-            <a href='#' onClick={handleLinkClick}>
+            <a href={link}>
                 <h3 className='card-heading'>{heading}</h3>
             </a>
             <p className='card-description'>{description}</p>
